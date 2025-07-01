4269b7086210b6f53509dcdc2997c725f58de361
Ravikanth Peddibhotla <rk.peddibhotla@gmail.com>
2025-07-01 10:30:44 -0700

    Completed Problem-1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,23 @@
+class Problem1{
+    public int findMiss(int[] nums){
+        int low=0,high=nums.length-1;
+        while(low<=high){
+            int mid = low +(high-low)/2;
+            if(nums[mid]-mid!=nums[0]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low + nums[0];
 
+    }
+
+    public static void main(String[]args){
+        Problem1 ob = new Problem1();
+        int[] nums = new int[]{1, 2, 3, 4, 6, 7, 8};
+        System.out.println("Missing element is: "+ob.findMiss(nums));
+
+    }
+}
